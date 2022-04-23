674e403fea922fe4134a304df2577a7681e11533
momoclouq <phamhoangminh050899@gmail.com>
2022-04-23 10:03:00 +0700

    first commit, ini repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>sdcasd</div>
+      <div className="sketchfab-embed-wrapper"> <iframe title="Chesspiece" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/e9c0b9d501204ddcacf8eb5e8600ce4e/embed"> </iframe> <p style={{fontsize: "13px", fontWeight: "normal", margin: "5px", color: "#4A4A4A"}}> <a href="https://sketchfab.com/3d-models/chesspiece-e9c0b9d501204ddcacf8eb5e8600ce4e?utm_medium=embed&utm_campaign=share-popup&utm_content=e9c0b9d501204ddcacf8eb5e8600ce4e" target="_blank" style={{fontWeight: "bold", color: "#1CAAD9"}}> Chesspiece </a> by <a href="https://sketchfab.com/historiska?utm_medium=embed&utm_campaign=share-popup&utm_content=e9c0b9d501204ddcacf8eb5e8600ce4e" target="_blank" style={{ fontWeight: "bold", color: "#1CAAD9"}}> Historiska </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=e9c0b9d501204ddcacf8eb5e8600ce4e" target="_blank" style={{fontWeight: "bold", color: "#1CAAD9"}}>Sketchfab</a></p></div>
     </div>
   );
 }
