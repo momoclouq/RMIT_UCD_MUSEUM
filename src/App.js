3bcaeb71b754aa142f1faca962ea04f49d324259
momoclouq <phamhoangminh050899@gmail.com>
2022-04-23 18:43:03 +0700

    Add support for tailwind css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>sdcasd</div>
-      <div className="sketchfab-embed-wrapper"> <iframe title="Chesspiece" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/e9c0b9d501204ddcacf8eb5e8600ce4e/embed"> </iframe> <p style={{fontsize: "13px", fontWeight: "normal", margin: "5px", color: "#4A4A4A"}}> <a href="https://sketchfab.com/3d-models/chesspiece-e9c0b9d501204ddcacf8eb5e8600ce4e?utm_medium=embed&utm_campaign=share-popup&utm_content=e9c0b9d501204ddcacf8eb5e8600ce4e" target="_blank" style={{fontWeight: "bold", color: "#1CAAD9"}}> Chesspiece </a> by <a href="https://sketchfab.com/historiska?utm_medium=embed&utm_campaign=share-popup&utm_content=e9c0b9d501204ddcacf8eb5e8600ce4e" target="_blank" style={{ fontWeight: "bold", color: "#1CAAD9"}}> Historiska </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=e9c0b9d501204ddcacf8eb5e8600ce4e" target="_blank" style={{fontWeight: "bold", color: "#1CAAD9"}}>Sketchfab</a></p></div>
+      <div className="sketchfab-embed-wrapper"> <iframe title="Chesspiece" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/e9c0b9d501204ddcacf8eb5e8600ce4e/embed"> </iframe> </div>
+      <h1 className="text-3xl font-bold underline bg-gray-100">
+        Hello world!
+      </h1>
     </div>
   );
 }
